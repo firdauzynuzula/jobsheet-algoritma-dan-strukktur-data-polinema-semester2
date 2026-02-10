38d66f022979e85c9d9329b679bb474ccee5b1ce
firdauzy <nuzulafirdhauzy@gmail.com>
2026-02-10 15:21:56 +0700

    Mengubah file Tugas1.java

--- Tugas1.java
+++ Tugas1.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
+
 public class Tugas1 {
     public static void main(String[] args) {
-        
-    }               
-    
+        Scanner input = new Scanner(System.in);
+
+        // Array satu dimensi char KODE[10] - kode plat mobil
+        char[] KODE = {'A', 'B', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T'};
+
+        // Array dua dimensi char KOTA[10][12] - nama kota
+        char[][] KOTA = {
+            {'B', 'A', 'N', 'T', 'E', 'N', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'J', 'A', 'K', 'A', 'R', 'T', 'A', ' ', ' ', ' ', ' ', ' '},
+            {'B', 'A', 'N', 'D', 'U', 'N', 'G', ' ', ' ', ' ', ' ', ' '},
+            {'C', 'I', 'R', 'E', 'B', 'O', 'N', ' ', ' ', ' ', ' ', ' '},
+            {'B', 'O', 'G', 'O', 'R', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'E', 'K', 'A', 'L', 'O', 'N', 'G', 'A', 'N', ' ', ' '},
+            {'S', 'E', 'M', 'A', 'R', 'A', 'N', 'G', ' ', ' ', ' ', ' '},
+            {'S', 'U', 'R', 'A', 'B', 'A', 'Y', 'A', ' ', ' ', ' ', ' '},
+            {'M', 'A', 'L', 'A', 'N', 'G', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'T', 'E', 'G', 'A', 'L', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+        };
+
+        // Input pencarian wilayah berdasarkan kode plat
+        System.out.print("Masukkan kode plat kendaraan: ");
+        char cari = input.nextLine().toUpperCase().trim().charAt(0);
+
+        boolean ditemukan = false;
+        for (int i = 0; i < KODE.length; i++) {
+            if (KODE[i] == cari) {
+                System.out.print("Kode plat " + KODE[i] + " adalah wilayah ");
+                for (int j = 0; j < KOTA[i].length; j++) {
+                    System.out.print(KOTA[i][j]);
+                }
+                System.out.println();
+                ditemukan = true;
+                break;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Kode plat '" + cari + "' tidak ditemukan dalam data.");
+        }
+
+        input.close();
+    }
 }
